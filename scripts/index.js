c0080c015564a7c51a88f2bc1b356b8457d4591c
Aleks Shchepinov <shepinoff.sasha@yandex.ru>
2025-01-19 23:45:58 +0400

    PR5 ver1.0 iter #1 19.01.2025

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
-// @todo: Темплейт карточки
+function createTemplates(item) {
+  const templates = document
+    .querySelector("#card-template")
+    .content.cloneNode(true);
 
-// @todo: DOM узлы
+  const cardName = templates.querySelector(".card__title");
+  const cardImage = templates.querySelector(".card__image");
+  const deleteButton = templates.querySelector(".card__delete-button");
 
-// @todo: Функция создания карточки
+  cardName.textContent = item.name;
+  cardImage.src = item.link;
 
-// @todo: Функция удаления карточки
+  deleteButton.addEventListener("click", function () {
+    const removeItem = deleteButton.closest(".places__item");
+    removeItem.remove();
+  });
 
-// @todo: Вывести карточки на страницу
+  return templates;
+}
+
+function addTemplates(cards) {
+  const placesList = document.querySelector(".places__list");
+  cards.forEach(function (item) {
+    const templates = createTemplates(item);
+
+    placesList.append(templates);
+  });
+}
+
+addTemplates(initialCards);
